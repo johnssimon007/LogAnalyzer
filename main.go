f8cb5945e92e0a5a367760319325703de25b4973
johnssimon007 <johnssimon007@gmail.com>
2021-08-12 08:02:33 +0530

    changes

--- main.go
+++ main.go
@@ -4,82 +4,77 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"github.com/common-nighthawk/go-figure"
 	"os"
-  "regexp"
-  "github.com/common-nighthawk/go-figure"
+	"regexp"
 )
 
-func LogAnalyze(fn string,content string) {
+func LogAnalyze(fn string, content string) {
 
-  var pattern = regexp.MustCompile(content)
+	var pattern = regexp.MustCompile(content)
 
 	r, err := os.Open(fn)
 	if err == nil {
-    files,err:=r.Readdir(0)
-    if err != nil {
-      fmt.Println(err)
-    }
-    for _,v:=range files{
-      reads, err := os.Open(fn+"/"+v.Name())
-      if err != nil {
-        fmt.Println(err)
-      }
-      sc := bufio.NewScanner(reads)
+		files, err := r.Readdir(0)
+		if err != nil {
+			fmt.Println(err)
+		}
+		for _, v := range files {
+			reads, err := os.Open(fn + "/" + v.Name())
+			if err != nil {
+				fmt.Println(err)
+			}
+			sc := bufio.NewScanner(reads)
 
-      for sc.Scan() {
-        result:=pattern.FindAllString(sc.Text(),-1)
-      if len(result)!=0{
+			for sc.Scan() {
+				result := pattern.FindAllString(sc.Text(), -1)
+				if len(result) != 0 {
 
-        if result[0]!=""{
+					if result[0] != "" {
 
-          fmt.Printf(string("\033[31m Found %v match in %s \033[34m %s \n"),len(result),fn+v.Name(),result)
+						fmt.Printf(string("\033[31m Found %v match in %s \033[34m %s \n"), len(result), fn+v.Name(), result)
 
+					}
 
+				}
+			}
 
-        }
-
-
-      }
-    }
-
-
-					reads.Close()
-    }
-
+			reads.Close()
+		}
 
 		r.Close()
 
 	}
 
 }
-func regex_file(fn string){
-  read, err := os.Open("regex.conf")
+func regex_file(fn string) {
+	read, err := os.Open("regex.conf")
 	if err == nil {
 		file_read := bufio.NewScanner(read)
 
 		for file_read.Scan() {
 
-      content:=file_read.Text()
-      var comment = regexp.MustCompile(`^#.*`)
-      var check bool=comment.MatchString(content)
+			content := file_read.Text()
+			var comment = regexp.MustCompile(`^#.*`)
+			var check bool = comment.MatchString(content)
 
-             if !check{
-      LogAnalyze(fn,content)
+			if !check {
+				LogAnalyze(fn, content)
 
-}
+			}
 
 		}
 
 		read.Close()
 	} else {
-    panic(err)
-  }
+		panic(err)
+	}
 
 }
 func main() {
 
-  ascii := figure.NewColorFigure("Log analyzer", "", "green", true)
-  ascii.Print()
+	ascii := figure.NewColorFigure("Log analyzer", "", "green", true)
+	ascii.Print()
 	flag.Parse()
 	fn := flag.Arg(0)
 	if fn == "" {
@@ -87,7 +82,6 @@ func main() {
 		os.Exit(1)
 	} else {
 		regex_file(fn)
-
 
 	}
 }
